d29700600f6e2608c4cc7da74163b599c13954f5
Matheus Rosisca Padovani <MatheusPadovani@users.noreply.github.com>
2020-05-11 16:46:25 -0300

    Menu criado
    Criei um menu genérico que posso mudar depois q escolher nome backgtound e cores

---
+++ Iniciar.gd
@@ -0,0 +1,7 @@
+extends Button
+
+func _on_Iniciar_pressed():
+	get_tree().change_scene("res://IsometricExample.tscn")
+	pass # Replace with function body.
+	
+
---
+++ Sair.gd
@@ -0,0 +1,5 @@
+extends Button
+
+func _on_Sair_pressed():
+	get_tree().quit()
+	pass 
