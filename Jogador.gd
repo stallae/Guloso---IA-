0782865778f109e1a9e0e9d1b1b9edbecf45f2c8
HeavyBR <matheus.cumpian@hotmail.com>
2020-05-14 18:40:04 -0300

    [Task 4] Criando movimento na grid

---
+++ Jogador.gd
@@ -0,0 +1,61 @@
+extends Area2D
+
+#Referência para o nó de RayCast
+onready var ray: RayCast2D = $"SensorDeColisão"
+
+onready var debugRay: Line2D = $Sprite/Line2D
+
+onready var debugTimer: Timer = $Sprite/Line2D/Timer
+
+onready var tween: Tween = $Tween
+
+export var debugMode = false
+
+#Tamanho de cada tile do GRID
+export var tile_size: int = 64 #Alterar conforme tamanho das tiles
+
+var velocidade: float = 3.0
+
+#Possíveis movimentações do usuário
+var inputs: Dictionary = {
+	"ui_right": Vector2.RIGHT,
+	"ui_left": Vector2.LEFT,
+	"ui_up": Vector2.UP,
+	"ui_down": Vector2.DOWN,
+}
+
+func _ready() -> void:
+	position.snapped(Vector2.ONE * tile_size)
+	position += Vector2.ONE * tile_size / 2
+	
+func _unhandled_input(event) -> void:
+	if tween.is_active():
+		return
+	for dir in inputs.keys():
+		if event.is_action_pressed(dir):
+			move(dir)
+
+func move(dir) -> void:
+	if(!is_colliding(dir)):
+		tween.interpolate_property(self, "position", position,
+		position + inputs.get(dir) * tile_size, 1.0/velocidade, Tween.TRANS_LINEAR,
+		Tween.EASE_IN_OUT)
+		tween.start()
+	
+func is_colliding(dir) -> bool:
+	if(debugMode):
+		debugRay.add_point(Vector2.ZERO)
+		debugRay.add_point(inputs[dir] * tile_size)
+		debugTimer.start(0.3)
+	ray.cast_to = inputs[dir] * tile_size
+	ray.force_raycast_update()
+	if !ray.is_colliding():
+		return false
+	else:
+		return true
+
+func remove_line_points() -> void: 
+	debugRay.points = []
+
+func _on_Timer_timeout():
+	remove_line_points()
