c4fe8c96b4e1eced24937f1cea55ed7e3eddab74
Matheus Cumpian <matheus.cumpian@sensedia.com>
2020-06-11 20:43:03 -0300

    Animações, camera, menu

--- Scenes/End.gd
+++ Scenes/End.gd
@@ -5,10 +5,19 @@ onready var resultado = get_node("textoResultado")
 
 func _ready():
 	print(grid)
-	var resultadoIa = grid.astar_path.size()
+	var resultadoIa = counter(grid.astar_path)
 	var resultadoPlayer = grid.player_movements 
-	var placar = " Placar: " + str(resultadoPlayer) + " " + str(resultadoIa)
+	var placar = " Placar: " + str(resultadoPlayer) + " contra " + str(resultadoIa) + " da IA."
 	if(resultadoPlayer <= resultadoIa):
 		resultado.text = "Resultado: Voce ganhou da nossa IA!!!" + str(placar)
 	else:
 		resultado.text = "Resultado: Voce perdeu da nossa IA!!!" + str(placar) 
+
+
+
+func counter(array) -> int:
+	var result = 0
+	for i in range(array.size()):
+		for j in array[i]:
+			result += 1
+	return result
